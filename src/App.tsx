5084da6a8c397ba250fa828e845bc48a3833242d
dtoledano1 <117308197+dtoledano1@users.noreply.github.com>
2025-08-13 10:42:46 +0300

    Revert "Add Floating Action Button for bug reports in App component (#4)"
    This reverts commit dfc7871ed5e164ba734b7c458c4b61034a7c0804.

--- src/App.tsx
+++ src/App.tsx
@@ -219,14 +219,6 @@ function App() {
           <small>Current tab: {currentTab}</small>
         </div>
       </main>
-      
-      <button 
-        className="fab-button report-bug-fab"
-        onClick={() => window.open('https://github.com/guy-meiri-microsoft/genni/issues/new', '_blank')}
-        title="Report a bug"
-      >
-        🐛
-      </button>
     </div>
   );
 }
